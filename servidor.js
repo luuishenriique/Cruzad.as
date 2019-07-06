3f7b6ae3be2879ea321716117848c8281766cb9f
Luis Henrique <luis_henrique_co@hotmail.com>
2019-07-05 23:09:02 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,27 +1,73 @@
-var http = require('http'); //Chamadas de variáves importadas do node para a criação do servidor
-var url = require('url');
-var fs = require('fs');
+const express = require('express');
+const session = require('express-session');
 
-//Criação do servidor
-http.createServer(function (req,res) {
+const host = 'localhost';
+const porta = 3000;
 
-    var q = url.parse(req.url, true); //Variável para armazenamento dos valores da url
-    var filename = "." + q.pathname; 
-    
-    //Chamada do arquivo e condição para abertura
-    fs.readFile(filename, function(err,data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end('URL indefinida!');
-        }
+var app = express();
 
-        res.write(data);
-        res.end();
-    });
+app.use(express.static('public'));
+
+app.use(session({ secret: 'XXassasas¨¨$', resave: false, saveUninitialized: true }));
+
+// entre em localhost:3000 para escrever os dados na sessao
+
+app.get('/', function(req, res, next) {
+	 res.sendFile('index.html' , { root : __dirname});
+});
+
+app.get('/index.html', function(req, res, next) {
+	 res.sendFile('index.html' , { root : __dirname});
+});
+
+app.get('/sobre.html', function(req, res, next) {
+	res.sendFile('sobre.html' , { root : __dirname});
+});
+
+app.get('/ajuda.html', function(req, res, next) {
+	res.sendFile('ajuda.html' , { root : __dirname});
+});
+
+app.get('/apresentacao.html', function(req, res, next) {
+	res.sendFile('apresentacao.html' , { root : __dirname});
+});
+
+app.get('/frutas.html', function(req, res, next) {
+	res.sendFile('frutas.html' , { root : __dirname});
+});
+
+app.get('/historia.html', function(req, res, next) {
+	res.sendFile('historia.html' , { root : __dirname});
+});
+
+app.get('/geral.html', function(req, res, next) {
+	res.sendFile('geral.html' , { root : __dirname});
+});
+
+app.listen(3000, () => {
+  console.log('Escutando localhost:3000');
 })
 
-//Chamada da porta e aviso de uso
-.listen(8080, function() {
-    console.log('Servidor ativo no endereço: http://localhost:8080');
-    console.log('Para desativar pressione ctlr + c');    
-});
+var arrayresposta = 
+
+['A' ,'C' ,'E' ,'R' ,'O' ,'L' ,'A' ,'' ,'' ,'T',
+'' ,'A' ,'' ,'' ,'' ,'I' ,'' ,'M' ,'' ,'O',
+'' ,'C' ,'' ,'M' ,'' ,'M' ,'' ,'A' ,'' ,'M',
+'B' ,'A' ,'N' ,'A' ,'N' ,'A' ,'' ,'M' ,'' ,'A',
+'' ,'U' ,'' ,'Ç' ,'' ,'O' ,'' ,'A' ,'' ,'T',
+'U' ,'' ,'' ,'A' ,'' ,'' ,'' ,'O' ,'' ,'E',
+'V' ,'' ,'' ,'' ,'' ,'' ,'J' ,'' ,'' ,'',
+'A' ,'' ,'' ,'L' ,'A' ,'R' ,'A' ,'N' ,'J' ,'A',
+'' ,'' ,'' ,'' ,'' ,'' ,'C' ,'' ,'' ,'',
+'K' ,'I' ,'W' ,'I' ,'' ,'C' ,'A' ,'J' ,'U' ,'']
+
+app.post('/gabarito', (req,res) => {
+	const {resposta} = req.body;
+	if (resposta == arrayresposta) {
+		res.write("<h1> Parabéns, você conseguiu completar as palavras! :D <h1>");
+	}else{
+		res.write("<h1> Algumas coisas estão faltando, tente novamente!");
+	}
+})
+
+
